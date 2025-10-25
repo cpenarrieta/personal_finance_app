683712d577c5604e966950c2f720ef726ec087f1
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2025-10-24 23:07:09 -0700

    new prisma config to be able to use both local database and prisma hosted one (with accelerate)

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-require-imports */
 // lib/prisma.ts
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
+const databaseUrl = process.env.DATABASE_URL || ''
+const useAccelerate = databaseUrl.startsWith('prisma://') || databaseUrl.startsWith('prisma+postgres://')
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const globalForPrisma = global as unknown as { prisma: any }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+let prismaInstance: any
+
+if (useAccelerate) {
+  // Production: Use Prisma Accelerate
+  const { PrismaClient } = require('@prisma/client/edge')
+  const { withAccelerate } = require('@prisma/extension-accelerate')
+  prismaInstance = new PrismaClient().$extends(withAccelerate())
+} else {
+  // Local Development: Use standard PrismaClient
+  const { PrismaClient } = require('@prisma/client')
+  prismaInstance = new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+  })
+}
+
+export const prisma = globalForPrisma.prisma || prismaInstance
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
