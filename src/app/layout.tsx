a98285d3fcc2999a6742d6ae76597974bc4bbe5d
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2025-11-05 20:42:47 -0800

    move auth check to proxy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { AuthGuard } from "@/components/AuthGuard";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
@@ -43,7 +42,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <AuthGuard>{children}</AuthGuard>
+          {children}
           <Toaster />
         </ThemeProvider>
       </body>
